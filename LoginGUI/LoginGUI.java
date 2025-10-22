137eab4ed17c050cd7bb6e98d1e740fc198eef4a
ahmakram2005-alt <ahmakram.2005@gmail.com>
2025-10-22 16:49:15 +0300

    added actionlistener

--- LoginGUI/LoginGUI.java
+++ LoginGUI/LoginGUI.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 public class LoginGUI{
 private static JFrame frame;
- public static void main(String[] args) {
+public static void main(String[] args) {
 frame = new JFrame("Login Form");
 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 frame.setSize(400, 300);
@@ -13,12 +13,17 @@ frame.setLayout(new GridLayout(3, 2, 10, 10));
 JTextField usernameField = new JTextField();
 JLabel passwordLabel = new JLabel("Password:");
 JPasswordField passwordField = new JPasswordField();
-  JButton loginButton = new JButton("Login");
+JButton loginButton = new JButton("Login");
 frame.add(usernameLabel);
 frame.add(usernameField);
 frame.add(passwordLabel);
 frame.add(passwordField);
 frame.add(new JLabel()); 
 frame.add(loginButton);
+loginButton.addActionListener(new ActionListener() {
+public void actionPerformed(ActionEvent e) {
+String username = usernameField.getText();
+char[] password = passwordField.getPassword();
+
 
 
