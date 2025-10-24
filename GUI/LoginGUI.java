7bd99dcff29d0ab012aa86d6b8db349a2b3b994c
Ziad Gebril <zgebro2005@gmail.com>
2025-10-25 02:40:05 +0300

    Edited log in menu isung new method : GridBagConstraints

--- GUI/LoginGUI.java
+++ GUI/LoginGUI.java
@@ -18,19 +18,57 @@ public class LoginGUI {
    public void display() {
         frame = new JFrame("Login Form");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 500);
-        frame.setLayout(new GridLayout(3, 2, 10, 10));
-        JLabel usernameLabel = new JLabel("Username:");
+        frame.setSize(1000, 500);
+        frame.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();  //function that we use to align using X,y,width
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        Dimension buttonSize = new Dimension(120, 40);
+        Dimension fieldSize = new Dimension(400, 40);
+
+        JLabel usernameLabel = new JLabel("Username");
         JTextField usernameField = new JTextField();
-        JLabel passwordLabel = new JLabel("Password:");
+        usernameField.setPreferredSize(fieldSize);
+
+        JLabel passwordLabel = new JLabel("Password");
         JPasswordField passwordField = new JPasswordField();
+        passwordField.setPreferredSize(fieldSize);
+
         JButton loginButton = new JButton("Login");
-        frame.add(usernameLabel);
-        frame.add(usernameField);
-        frame.add(passwordLabel);
-        frame.add(passwordField);
-        frame.add(new JLabel());
-        frame.add(loginButton);
+        loginButton.setPreferredSize(buttonSize);
+
+        // Username label
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        frame.add(usernameLabel, gbc);
+
+        // Username field
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        frame.add(usernameField, gbc);
+
+        // Password label
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        frame.add(passwordLabel, gbc);
+
+        // Password field
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        frame.add(passwordField, gbc);
+
+        // Login button
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        frame.add(loginButton, gbc);
+
         loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String username = usernameField.getText();
