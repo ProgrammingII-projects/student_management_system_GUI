9109ea0507ed4a20f4b9ec9101d3dc2fc1a1a926
Ziad Gebril <zgebro2005@gmail.com>
2025-10-25 20:18:26 +0300

    student widget was created

--- view/studentWidget.java
+++ view/studentWidget.java
@@ -1,43 +1,53 @@
 package view;
+
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-import javax.swing.JButton;
-
 import model.*;
-public class studentWidget {
-    
-    private int studentID;
-    private String Name;
-    private double GPA;
 
-    public studentWidget(Student st)
-    {
-        GPA = st.getGPA();
-        Name = st.getName();
-        studentID = st.getStudentID();
+public class studentWidget {
+
+    private Student student;
+    private JPanel buttonPanel;
+    private Dimension buttonSize;
+    private static JFrame frame;
+
+    public studentWidget(Student st,JFrame frame) {
+        this.student = st;
+        this.buttonPanel = new JPanel();
+        this.frame = frame;
+       
+        setupPanel();
     }
 
-    public void display()
-    {
-        
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 30));
-        
-        JButton button = new JButton(studentID + "   " + Name + "              " + GPA);
+    // Setup the student widget display panel (single button with student summary)
+    private void setupPanel() {
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 30));
+        String label = student.getStudentID() + "   " + student.getName() + "              " + student.getGPA();
+        JButton button = new JButton(label);
         Dimension buttonSize = new Dimension(400, 40);
         button.setPreferredSize(buttonSize);
         buttonPanel.add(button);
-        
+    }
 
+    // Return the widget's JPanel for use in other views (e.g., displayStudent)
+    public JPanel getPanel() {
+        return buttonPanel;
     }
 
 
+      
+        // Display the student widget (summary) above the buttons
+        public void display(){
+            Dimension buttonSize = new Dimension(900, 40);
+        frame.add(getPanel());
+        frame.add(buttonPanel);
+        buttonPanel.setPreferredSize(this.buttonSize);
+        
+        frame.setVisible(true);
 
-
-
+        }
 
 }
